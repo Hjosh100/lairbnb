f0b99f3ae54dfd433c2b12c818417f7b519c11eb
Samuel Anderson <samuelSD.999@gmail.com>
2024-02-17 16:37:42 +0000

    Edit association in lair and edit booking routes

--- app/models/lair.rb
+++ app/models/lair.rb
@@ -3,7 +3,7 @@ class Lair < ApplicationRecord
   validates :category, precence: true, inclusion: { in: %w[island mountain countryside space underground] }
   validates :location, presence: true
   validates :price, presence: true
-  has_one_attached :photo
+  has_many_attached :photos
   belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
