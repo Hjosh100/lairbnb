f340a0de6ca219030f955d5be7f9eeb74da441a4
Josue Heredia <josue-heredia@outlook.com>
2024-02-17 15:13:05 +0000

    added new.html.erb to lairs view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "lairs#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :bookings do
   end
-  
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
