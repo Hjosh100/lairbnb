3f51cb2195e8a1045c97ed25f8d899717b4d9d07
Kieran Lambe <lambekieran@me.com>
2024-02-18 23:12:35 +0000

    started fiddling with the buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_02_17_162834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,10 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_17_162834) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-=======
-ActiveRecord::Schema[7.1].define(version: 2024_02_17_160501) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.boolean "accepted"
@@ -55,7 +51,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_17_160501) do
     t.datetime "updated_at", null: false
     t.index ["lair_id"], name: "index_bookings_on_lair_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
-
   end
 
   create_table "lairs", force: :cascade do |t|
@@ -83,12 +78,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_17_160501) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "bookings", "lairs"
   add_foreign_key "bookings", "users"
-
   add_foreign_key "lairs", "users"
 end
