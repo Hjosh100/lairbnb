c3c2627865317d489826cf077da0a21130844859
Samuel Anderson <samuelSD.999@gmail.com>
2024-02-18 04:48:43 +0000

    Set accepted to be false by default through migrations

--- db/migrate/20240217160501_create_bookings.rb
+++ db/migrate/20240217160501_create_bookings.rb
@@ -1,7 +1,7 @@
 class CreateBookings < ActiveRecord::Migration[7.1]
   def change
     create_table :bookings do |t|
-      t.boolean :accepted, default: false
+      t.boolean :accepted
       t.date :date
       t.references :user, null: false, foreign_key: true
       t.references :lair, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_18_024300) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_18_044618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,12 +43,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_18_024300) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.boolean "accepted", default: false
     t.date "date"
     t.bigint "user_id", null: false
     t.bigint "lair_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "accepted", default: false
     t.index ["lair_id"], name: "index_bookings_on_lair_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
