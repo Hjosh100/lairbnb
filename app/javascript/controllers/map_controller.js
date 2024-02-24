55059bb9a84f19180a0055e1bb27600b48055e2a
Kieran Lambe <lambekieran@me.com>
2024-02-24 16:43:43 +0000

    style added to map, more yet to come

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10",
+      style: "mapbox://styles/konsite/clt06a02r003t01p82ysrcz93",
     });
 
     this.#addMarkersToMap();
